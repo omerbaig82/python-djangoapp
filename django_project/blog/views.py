271d5de1897ec648a8cff072e29ed6cc6dba52e5
Omer Baig <Omer.Baig@willistowerswatson.com>
2022-06-28 15:47:15 +0000

    Database, migrations, Posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-
-posts = [
-    {
-        'author': 'OmerB',
-        'title': 'Blog Post 1',
-        'content': 'First post here',
-        'date_posted': 'June 27, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post here',
-        'date_posted': 'June 26, 2022'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1+d3q)r+v&xww+!2zm(&1-$ww_s19xz*r0jtq)gg#e!w56e+r%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.241.240.156', '54.75.48.237']
+ALLOWED_HOSTS = ['34.241.240.156', '54.75.48.237', '34.255.6.160', '34.255.6.85']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
