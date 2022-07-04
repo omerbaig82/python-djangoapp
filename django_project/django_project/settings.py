02d6be0379b1400538108a7aaf8bc5f76d4940df
Omer Baig <Omer.Baig@willistowerswatson.com>
2022-07-04 16:12:20 +0000

    User registration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = [IP_ADDRESS]
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
